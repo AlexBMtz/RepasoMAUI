1abc17d6b331d04e5dc2b4fa23e2700f83405ff6
AlexBMtz <barroetaalejandro160401@gmail.com>
2026-08-22 23:59:50 -0600

    se crea el viewModel de favoritos

---
+++ ViewModels/FavoritosViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RepasoMAUI.Data;
+using RepasoMAUI.Models;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Linq;
+
+namespace RepasoMAUI.ViewModels
+{
+    public partial class FavoritosViewModel : ObservableObject
+    {
+
+        [ObservableProperty]
+        private ObservableCollection<Producto> favoritos = new();
+
+        
+        public void AgregarFavorito(Producto producto)
+        {
+            if (producto is null) return;
+            if (!Favoritos.Any(p => p.Id == producto.Id))
+                Favoritos.Add(producto);
+        }
+
+        [RelayCommand]
+        private void EliminarFavorito(Producto producto)
+        {
+            if (producto is null) return;
+            Favoritos.Remove(producto);
+        }
+
+    }
+}
