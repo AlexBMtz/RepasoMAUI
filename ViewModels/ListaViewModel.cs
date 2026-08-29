47b3ee9bf634bcb72ff09811e0c9f35c23d04d1e
Derek Ramirez <dreviasolutions@gmail.com>
2026-08-29 10:37:58 -0600

    Restaura VerDetalleCommand en ListaViewModel (faltaba en la base de feature/HttpClient)

--- ViewModels/ListaViewModel.cs
+++ ViewModels/ListaViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using RepasoMAUI.Models;
 using RepasoMAUI.Services;
+using RepasoMAUI.Views;
 using System.Collections.ObjectModel;
 
 namespace RepasoMAUI.ViewModels
@@ -48,6 +49,14 @@ namespace RepasoMAUI.ViewModels
             }
 
             IsLoading = false;
+        }
+
+        [RelayCommand]
+        static async Task VerDetalle(Producto producto)
+        {
+            if (producto is null) return;
+
+            await Shell.Current.GoToAsync($"/{nameof(DetallePage)}?id={producto.Id}");
         }
     }
 }
