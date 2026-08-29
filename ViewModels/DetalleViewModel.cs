56e737f153e68e2383ce8560924360de1ce9df6e
Juncal Serrano <juncalserrano04@gmail.com>
2026-08-29 00:26:13 -0600

    Se agrego a detalleViewModel lo de favoritos y en detallePage  el boton

--- ViewModels/DetalleViewModel.cs
+++ ViewModels/DetalleViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using RepasoMAUI.Data;
 using RepasoMAUI.Models;
 
@@ -8,6 +9,7 @@ namespace RepasoMAUI.ViewModels
     public partial class DetalleViewModel : ObservableObject
     {
         private readonly ProductoRepository _repo;
+        private readonly FavoritosViewModel _favoritosVM;
 
         [ObservableProperty]
         private string id;
@@ -15,9 +17,10 @@ namespace RepasoMAUI.ViewModels
         [ObservableProperty]
         private Producto producto;
 
-        public DetalleViewModel(ProductoRepository repo)
+        public DetalleViewModel(ProductoRepository repo, FavoritosViewModel favoritosVM)
         {
             _repo = repo;
+            _favoritosVM = favoritosVM;
         }
 
         // Se dispara automáticamente cuando Shell asigna el query property "id",
@@ -25,6 +28,13 @@ namespace RepasoMAUI.ViewModels
         partial void OnIdChanged(string value)
         {
             Producto = _repo.ObtenerPorId(value);
+        }
+
+        [RelayCommand]
+        private void AgregarFavorito()
+        {
+            if (Producto != null)
+                _favoritosVM.AgregarFavorito(Producto);
         }
 
     }
