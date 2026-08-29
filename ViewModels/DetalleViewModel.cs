b245cb856b5fd1f3e89ff1bcb9573b98136ff690
hishamrosales <hisham.rosales@gmail.com>
2026-08-28 22:07:57 -0600

    .

--- ViewModels/DetalleViewModel.cs
+++ ViewModels/DetalleViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using RepasoMAUI.Data;
 using RepasoMAUI.Models;
+using RepasoMAUI.Views;
 
 namespace RepasoMAUI.ViewModels
 {
@@ -27,5 +29,62 @@ namespace RepasoMAUI.ViewModels
             Producto = _repo.ObtenerPorId(value);
         }
 
+        [RelayCommand]
+        private async Task AgregarProducto()
+        {
+            if (ProductoSeleccionado == null)
+            {
+                await Shell.Current.DisplayAlert("Error", "No se ha seleccionado ningún producto.", "OK");
+                return;
+            }
+            if (!Favoritos.Contains(ProductoSeleccionado))
+            {
+                Favoritos.Add(ProductoSeleccionado);
+                await Shell.Current.DisplayAlert("Exito", $"Producto '{ProductoSeleccionado.Nombre}' agregado a favoritos.", "OK");
+            }
+            else
+            {
+                await Shell.Current.DisplayAlert("Información", "El producto ya está en favoritos.", "OK");
+                return;
+            }
+        }
+
+        [RelayCommand]
+        private async Task VerFavoritos()
+        {
+            if (Favoritos == null || Favoritos.Count == 0)
+            {
+                await Shell.Current.DisplayAlert("Información", "No hay productos en favoritos.", "OK");
+                return;
+            }
+
+            var parametros = new Dictionary<string, object>
+            {
+                { "Favoritos", Favoritos }
+            };
+
+            await Shell.Current.GoToAsync(nameof(FavoritosPage), parametros);
+        }
+
+
+        [RelayCommand]
+        private async Task EliminarProducto()
+        {
+            if (ProductoSeleccionado == null)
+            {
+                await Shell.Current.DisplayAlert("Error", "No se ha seleccionado ningún producto.", "OK");
+                return;
+            }
+            if (Favoritos.Contains(ProductoSeleccionado))
+            {
+                Favoritos.Remove(ProductoSeleccionado);
+                await Shell.Current.DisplayAlert("Exito", $"Producto '{ProductoSeleccionado.Nombre}' eliminado de favoritos.", "OK");
+            }
+            else
+            {
+                await Shell.Current.DisplayAlert("Información", "El producto no está en favoritos.", "OK");
+                return;
+            }
+        }
     }
 }
---
+++ ViewModels/FavoritosViewModel.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using RepasoMAUI.Models;
+using System.Collections.ObjectModel;
+
+namespace RepasoMAUI.ViewModels
+{
+    [QueryProperty(nameof(Favoritos), "Favoritos")]
+    public partial class FavoritosViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<Producto> favoritos;
+
+        public FavoritosViewModel()
+        {
+        }
+    }
+}
