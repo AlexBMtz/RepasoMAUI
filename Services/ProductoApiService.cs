af76f62ea9fc5994d7f04e60c4900b5c0d3f5c7b
DamianLZ <pablodamiantkd@gmail.com>
2026-09-01 00:04:04 -0600

    Final code

--- Services/ProductoApiService.cs
+++ Services/ProductoApiService.cs
@@ -13,7 +13,7 @@ public class ProductoApiService
     {
         _http = new HttpClient
         {
-            Timeout = TimeSpan.FromMilliseconds(1) // ← temporal, para provocar timeout
+            Timeout = TimeSpan.FromSeconds(8)
         };
     }
 
